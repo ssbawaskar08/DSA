381b8eed055734992faa2f728d98bcf3c5096d80
ssbawaskar08 <bawaskar.shreyas@gmail.com>
2025-08-15 17:50:28 +0530

    Pairs of element in Array

---
+++ Array/AC_code/PairsOfArray.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class PairsOfArray {
+    public static void printSA (int arr[]){
+        System.out.println("The Pairs's are: ");
+        for(int i=0;i<arr.length;i++){
+            for(int j = i+1;j<arr.length;j++){
+                System.out.print("["+arr[i]+","+arr[j]+"]\t");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int arr [] = new int [10];
+
+        Scanner sc = new Scanner(System.in);
+
+        for (int i=0;i<arr.length;i++){
+            System.out.print("please enter the number to be inserted at the position "+ (i+1) + ": ");
+            arr[i]=sc.nextInt();
+            System.out.println();
+        }
+        printSA(arr);
+    }
+}
